432bb31934ed234604114b574c443f0c60f1eebe
b-withers <brady.withers@gmail.com>
2018-08-21 16:36:58 -0600

    pseudo code and notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
-$(document).ready(function(){
-            $.getJSON("assets/data/data.json", function(data) {
-                console.log(data);
-                var applicant_data = '';
-                $.each(data.applicants, function(key, value){
-                    applicant_data += '<tr>';
-                    applicant_data += '<td>' + value.id + '</td>';
-                    applicant_data += '<td>' + value.name + '</td>';
-                    applicant_data += '<td>' + value.website + '<t/d>';
-                    applicant_data += '<td>' + value.cover_letter + '</td>';
-                    applicant_data += '</tr>'; 
-                });
-                $('#applicant_table').append(applicant_data);
-            });
+$(document).ready(function () {
+    $.getJSON("assets/data/data.json", function (data) {
+        //I can see all the individual arrays within the JSON file.
+        console.log(data.applicants);
+        console.log(data.jobs);
+        console.log(data.skills);
+        //showing basic data from JSON file is working.
+        var applicant_data = '';
+        $.each(data.applicants, function (key, value) {
+            applicant_data += '<tr>';
+            applicant_data += '<td>' + value.id + '</td>';
+            applicant_data += '<td>' + value.name + '</td>';
+            applicant_data += '<td>' + value.website + '<t/d>';
+            applicant_data += '<td>' + value.cover_letter + '</td>';
+            applicant_data += '</tr>';
         });
+        $('#applicant_table').append(applicant_data);
+        //WORKING ON JOINING / SORTING / MERGING DATA FIELDS
+    });
+});
